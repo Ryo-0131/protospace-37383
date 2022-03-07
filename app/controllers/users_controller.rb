dfca0499d01e394937a4e5d89feddbde5b1ef7c4
Ryo-0131 <kankichi01kantoku@gmail.com>
2022-03-07 23:33:06 +0900

    ご指摘の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:show]
 
   def create
   end
 
   def show
     @user = User.find(params[:id]) 
-    # @user = User.find_by(id: params[:user_id])
-    # @prototypes = @user.prototypes
+    @prototypes = @user.prototypes
    end
 
 
